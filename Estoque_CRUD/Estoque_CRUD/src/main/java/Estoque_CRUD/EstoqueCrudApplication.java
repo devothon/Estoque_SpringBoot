a7e98de3a85910bfc402d0ef77902bc0cd1e3f70
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-05-27 13:16:49 -0300

    Projeto quase completo, falta a implementação do record

---
+++ Estoque_CRUD/Estoque_CRUD/src/main/java/Estoque_CRUD/EstoqueCrudApplication.java
@@ -0,0 +1,13 @@
+package Estoque_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueCrudApplication.class, args);
+	}
+
+}
---
+++ Estoque_CRUD/Estoque_CRUD/src/test/java/Estoque_CRUD/EstoqueCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package Estoque_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
